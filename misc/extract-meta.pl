67cd6428d31ec9a497312b21b01d2632ae2a3dbe
Arne Binder <arne.b.binder@gmail.com>
2014-07-21 16:37:43 +0200

    - use getopt  - use basename  - parse only teiHeader

--- misc/extract-meta.pl
+++ misc/extract-meta.pl
@@ -39,16 +39,195 @@ use strict;
 use warnings;
 use XML::LibXML;
 use SQL::Abstract;
+use Getopt::Long;
+use File::Basename;
 use List::Util qw/reduce/;
 use utf8;
 
 my %parameter;
 my %data;
 my @warnings;
-$parameter{path} = shift;
+
+$parameter{file} = 'file';
+$parameter{id_book} = 0;
+$parameter{source} = 'source';
+
+GetOptions ("id=i" => \$parameter{id_book},    
+            "file=s"   => \$parameter{file},      
+            "source=s"  => \$parameter{source})   
+or die("Error in command line arguments\n");
+
+
 my $sql = SQL::Abstract->new();
 
 use constant EMPTY => "emptydummy";
+
+
+
+#DEBUG
+#print "PARAMETER:\n";
+#printMap(%parameter);
+
+#my $id = pop;
+
+my $parser = XML::LibXML->new(line_numbers => 1);
+
+open( my $fh, '<', $parameter{file} ) or die $!;
+my $xml = do { local $/; <$fh> };
+close $fh;
+my ( $header ) = $xml =~ /(<teiHeader>.*<\/teiHeader>)/s;
+my $dom = $parser->parse_string( $header );
+
+#my $dom = $parser->parse_file($parameter{file});
+my $root = $dom->documentElement;
+my $xc = XML::LibXML::XPathContext->new( $root );
+$xc->registerNs('tei', 'http://www.tei-c.org/ns/1.0');
+
+# set id
+if(exists $parameter{id_book}){
+	$data{id_book} = $parameter{id_book};
+}
+
+# get authors
+my @authorNodes = getNodes('//teiHeader/fileDesc/titleStmt/author');
+if(@authorNodes){
+	if(@authorNodes > 3){
+		push(@warnings, 'more than 3 authors found');
+	}
+	my $i = 0;
+	while(@authorNodes){
+		$i++;
+		my %authorData = getPersonData(shift(@authorNodes));
+		if(exists $authorData{surname}){ $data{'autor'.$i.'_prename'} = qut($authorData{surname});}
+		if(exists $authorData{forename}){ $data{'autor'.$i.'_lastname'} = qut($authorData{forename});}
+		if(exists $authorData{addName}){ $data{'autor'.$i.'_syn_names'} = qut($authorData{addName});}
+		if(exists $authorData{pnd}){ $data{'autor'.$i.'_pnd'} = qut($authorData{pnd});}
+	}	
+}
+
+# get translator
+my @translatorNodes = getNodes('//teiHeader/fileDesc/sourceDesc/biblFull/titleStmt/editor[@role="translator"]');
+if(@translatorNodes){
+	$data{uebersetzer} = qut(constructName(getPersonData($translatorNodes[0])));
+}
+
+# get publisher
+my @publisherNodes = getNodes('//teiHeader/fileDesc/sourceDesc/biblFull/titleStmt/editor[not(@role="translator")]');
+if(@publisherNodes){
+	$data{publisher} = qut(constructName(getPersonData($publisherNodes[0])));
+}
+
+
+
+$data{title} = qut(getContent('//teiHeader/fileDesc/titleStmt/title[@type="main"]'));
+$data{subtitle} = qut(getContent('//teiHeader/fileDesc/titleStmt/title[@type="sub"]'));
+
+$data{dta_pub_date} = getContent('//teiHeader/fileDesc/sourceDesc/biblFull/publicationStmt/date[@type="publication"]');
+$data{dta_pub_location} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/publicationStmt/pubPlace'));
+$data{dta_pub_verlag} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/publicationStmt/publisher/name'));
+
+$data{dta_bibl_angabe} = qut(getContent('//teiHeader/fileDesc/sourceDesc/bibl'));
+
+$data{year} = getContent('//teiHeader/fileDesc/sourceDesc/biblFull/publicationStmt/date[@type="creation"]', $data{'dta_pub_date'});
+$data{umfang} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/extent/measure[@type="pages"]'));
+$data{umfang_normiert} = getContent('//teiHeader/fileDesc/extent/measure[@type="images"]');
+
+$data{band_alphanum} = getContent('//teiHeader/fileDesc/titleStmt/title[@type="volume"]'); 
+$data{band_zaehlung} = getAttributeValue('//teiHeader/fileDesc/titleStmt/title[@type="volume"]','n'); 
+
+#TODO: dta_reihe... correct?
+$data{dta_reihe_titel} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/seriesStmt/title[@type="main"]'));
+$data{dta_reihe_jahrgang} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/seriesStmt/biblScope[@unit="volume"]'));
+$data{dta_reihe_band} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/seriesStmt/biblScope[@unit="issue"]'));
+$data{dta_seiten} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/seriesStmt/biblScope[@unit="pages"]'));
+$data{dta_comment2} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/notesStmt/note'));
+
+$data{ready} = qut(getContent(''));
+
+$data{language} = getAttributeValue('//teiHeader/profileDesc/langUsage/language', 'ident', 'deu');
+if($data{language} ne 'deu'){
+	push(@warnings, 'language not german');
+}
+
+#$parameter{file} =~ m!/?([^\/]+?)\.!;
+($data{dirname}) = basename($parameter{file}) =~ /(^[^\.]*)/;
+$data{dirname} = qut($data{dirname});
+
+$data{type} = qut(getAttributeValue('//teiHeader/fileDesc/sourceDesc/bibl','type'));
+$data{schriftart} = qut(getContent('//teiHeader/fileDesc/sourceDesc/msDesc/physDesc/typeDesc/p'));
+
+#TODO: Priorität korrekt?
+$data{prioritaet} = '1';
+$data{planung} = qut('');
+$data{startseite} = '0';
+
+$data{genre} = qut(getContent('//teiHeader/profileDesc/textClass/classCode[@scheme="http://www.deutschestextarchiv.de/doku/klassifikation#dtamain"]','null'));
+$data{untergenre} = qut(getContent('//teiHeader/profileDesc/textClass/classCode[@scheme="http://www.deutschestextarchiv.de/doku/klassifikation#dtasub"]','null'));
+$data{dwds_kategorie1} = qut(getContent('//teiHeader/profileDesc/textClass/classCode[@scheme="http://www.deutschestextarchiv.de/doku/klassifikation#dwds1main"]'));
+$data{dwds_unterkategorie1} = qut(getContent('//teiHeader/profileDesc/textClass/classCode[@scheme="http://www.deutschestextarchiv.de/doku/klassifikation#dwds1sub"]'));
+
+$data{createdate} = `date +'%d-%m-%Y %H:%M:%S'`;
+$data{createdate} =~ s/\n//g;
+
+$data{source} = qut($parameter{source});
+
+
+$data{resp} = '';
+my @respStmts = getNodes('//teiHeader/fileDesc/titleStmt/respStmt');
+while(@respStmts > 1){
+	$data{resp} = $data{resp}.shift(@respStmts)."\n";
+}
+
+#TODO: just one editorialDecl?? without <editorialDecl> </editorialDecl>!!!
+my @editorialDecls = getNodes('//teiHeader/encodingDesc/editorialDecl');
+if(@editorialDecls){
+	($data{txt}) = $editorialDecls[0] =~ /<editorialDecl>(.*)<\/editorialDecl>/s;
+}
+
+my @msIdentifiers = getNodes('//teiHeader/fileDesc/sourceDesc/msDesc/msIdentifier');
+if(@msIdentifiers){
+	$data{msidentifier} = $msIdentifiers[0];
+}
+
+my @licence = getNodes('//teiHeader/fileDesc/publicationStmt/availability/licence');
+if(@licence){
+	$data{license} = $licence[0];
+}
+
+#DEBUG
+#print "DATA:\n";
+#printMap(%data);
+#print "\n";
+
+
+$data{sql} = generateSqlInsert('book', 'id_book', 'title', 'subtitle', 'autor1_prename', 'autor1_lastname', 'autor1_syn_names', 'autor2_prename', 'autor2_lastname', 'autor2_syn_names', 'autor3_prename', 'autor3_lastname', 'autor3_syn_names', 'availability ', 'year', 'dta_pub_date', 'dta_pub_location', 'dta_pub_verlag','umfang', 'umfang_normiert', 'band_zaehlung', 'band_alphanum', 'autor1_pnd','autor2_pnd','autor3_pnd', 'dta_reihe_titel', 'dta_reihe_jahrgang', 'dta_reihe_band', 'dta_seiten', 'dta_bibl_angabe', 'ready', 'uebersetzer', 'dta_uebersetzer', 'publisher', 'dta_comment2');
+$data{sql} = $data{sql}.";\n".generateSqlInsert('metadaten','id_book', 'genre', 'type', 'untergenre', 'dirname', 'schriftart', 'prioritaet', 'planung', 'startseite', 'dwds_kategorie1', 'dwds_unterkategorie1');
+$data{sql} = $data{sql}.";\n".generateSqlInsert('sources','id_book', 'source'); 
+$data{sql} = $data{sql}.";\n".'INSERT INTO open_tasks (id_book, id_task, createdate) SELECT '.$data{id_book}.', max(id_task) +1, '.qut($data{createdate}).' FROM open_tasks;';
+
+#DEBUG
+#print generateSqlInsert('book', 'id_book', 'title', 'subtitle', 'autor1_prename', 'autor1_lastname', 'prioritaet');
+
+
+if(not @warnings){
+	# could add warning...
+	writeData('resp', 'txt', 'msidentifier', 'license', 'sql');	
+}
+
+if(@warnings){
+	print "WARNINGS:\n";
+	while(@warnings){
+		print shift(@warnings)."\n";		
+	}
+}else{
+	print $data{sql};	
+}
+
+
+
+#########################
+#       Functions       #
+#########################
 
 sub printMap{
 	my (%map) = @_;
@@ -64,27 +243,6 @@ sub printMap{
 	
 	print "\n";
 }
-
-
-while(@ARGV){
-	my @temp = split('=',shift);
-	
-	if(@temp==2){
-		$parameter{$temp[0]} = $temp[1];
-	}
-}
-
-#DEBUG
-#print "PARAMETER:\n";
-#printMap(%parameter);
-
-#my $id = pop;
-
-my $parser = XML::LibXML->new(line_numbers => 1);
-my $dom = $parser->parse_file($parameter{path});
-my $root = $dom->documentElement;
-my $xc = XML::LibXML::XPathContext->new( $root );
-$xc->registerNs('tei', 'http://www.tei-c.org/ns/1.0');
 
 
 sub getContent{
@@ -106,7 +264,7 @@ sub getContent{
 	return EMPTY;
 }
 
-sub getNodeContent(){
+sub getNodeContent{
 	my ($node) = @_;
 	my $result = $node->getContent;
 	$result =~ s/^\s+|\s+$//g;
@@ -226,9 +384,9 @@ sub writeData{
 	if(exists $data{dirname}){
 		while(@_){
 			my $dataIndex = shift;
-			open(FILE, '>'.unqut($data{dirname}).'.'.$dataIndex);
-			print FILE $data{$dataIndex};
-			close(FILE);
+			open(FILE, '>'.unqut($data{dirname}).'.'.$dataIndex) or die $!;
+			print FILE $data{$dataIndex} or die $!;
+			close(FILE) or die $!;
 		}
 	}else{
 		push(@warnings, "dirname not set.")
@@ -284,144 +442,7 @@ sub generateSqlInsert{
 
 
 
-# set id
-if(exists $parameter{id_book}){
-	$data{id_book} = $parameter{id_book};
-}
 
-# get authors
-my @authorNodes = getNodes('//teiHeader/fileDesc/titleStmt/author');
-if(@authorNodes){
-	if(@authorNodes > 3){
-		push(@warnings, 'more than 3 authors found');
-	}
-	my $i = 0;
-	while(@authorNodes){
-		$i++;
-		my %authorData = getPersonData(shift(@authorNodes));
-		if(exists $authorData{surname}){ $data{'autor'.$i.'_prename'} = qut($authorData{surname});}
-		if(exists $authorData{forename}){ $data{'autor'.$i.'_lastname'} = qut($authorData{forename});}
-		if(exists $authorData{addName}){ $data{'autor'.$i.'_syn_names'} = qut($authorData{addName});}
-		if(exists $authorData{pnd}){ $data{'autor'.$i.'_pnd'} = qut($authorData{pnd});}
-	}	
-}
-
-# get translator
-my @translatorNodes = getNodes('//teiHeader/fileDesc/sourceDesc/biblFull/titleStmt/editor[@role="translator"]');
-if(@translatorNodes){
-	$data{uebersetzer} = qut(constructName(getPersonData($translatorNodes[0])));
-}
-
-# get publisher
-my @publisherNodes = getNodes('//teiHeader/fileDesc/sourceDesc/biblFull/titleStmt/editor[not(@role="translator")]');
-if(@publisherNodes){
-	$data{publisher} = qut(constructName(getPersonData($publisherNodes[0])));
-}
-
-
-
-$data{title} = qut(getContent('//teiHeader/fileDesc/titleStmt/title[@type="main"]'));
-$data{subtitle} = qut(getContent('//teiHeader/fileDesc/titleStmt/title[@type="sub"]'));
-
-$data{dta_pub_date} = getContent('//teiHeader/fileDesc/sourceDesc/biblFull/publicationStmt/date[@type="publication"]');
-$data{dta_pub_location} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/publicationStmt/pubPlace'));
-$data{dta_pub_verlag} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/publicationStmt/publisher/name'));
-
-$data{dta_bibl_angabe} = qut(getContent('//teiHeader/fileDesc/sourceDesc/bibl'));
-
-$data{year} = getContent('//teiHeader/fileDesc/sourceDesc/biblFull/publicationStmt/date[@type="creation"]', $data{'dta_pub_date'});
-$data{umfang} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/extent/measure[@type="pages"]'));
-$data{umfang_normiert} = getContent('//teiHeader/fileDesc/extent/measure[@type="images"]');
-
-$data{band_alphanum} = getContent('//teiHeader/fileDesc/titleStmt/title[@type="volume"]'); 
-$data{band_zaehlung} = getAttributeValue('//teiHeader/fileDesc/titleStmt/title[@type="volume"]','n'); 
-
-#TODO: dta_reihe... correct?
-$data{dta_reihe_titel} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/seriesStmt/title[@type="main"]'));
-$data{dta_reihe_jahrgang} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/seriesStmt/biblScope[@unit="volume"]'));
-$data{dta_reihe_band} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/seriesStmt/biblScope[@unit="issue"]'));
-$data{dta_seiten} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/seriesStmt/biblScope[@unit="pages"]'));
-$data{dta_comment2} = qut(getContent('//teiHeader/fileDesc/sourceDesc/biblFull/notesStmt/note'));
-
-$data{ready} = qut(getContent(''));
-
-$data{language} = getAttributeValue('//teiHeader/profileDesc/langUsage/language', 'ident', 'deu');
-if($data{language} ne 'deu'){
-	push(@warnings, 'language not german');
-}
-
-$parameter{path} =~ m!/?([^\/]+?)\.!;
-$data{dirname} = qut($1);
-
-$data{type} = qut(getAttributeValue('//teiHeader/fileDesc/sourceDesc/bibl','type'));
-$data{schriftart} = qut(getContent('//teiHeader/fileDesc/sourceDesc/msDesc/physDesc/typeDesc/p'));
-
-#TODO: Priorität korrekt?
-$data{prioritaet} = '1';
-$data{planung} = qut('');
-$data{startseite} = '0';
-
-$data{genre} = qut(getContent('//teiHeader/profileDesc/textClass/classCode[@scheme="http://www.deutschestextarchiv.de/doku/klassifikation#dtamain"]','null'));
-$data{untergenre} = qut(getContent('//teiHeader/profileDesc/textClass/classCode[@scheme="http://www.deutschestextarchiv.de/doku/klassifikation#dtasub"]','null'));
-$data{dwds_kategorie1} = qut(getContent('//teiHeader/profileDesc/textClass/classCode[@scheme="http://www.deutschestextarchiv.de/doku/klassifikation#dwds1main"]'));
-$data{dwds_unterkategorie1} = qut(getContent('//teiHeader/profileDesc/textClass/classCode[@scheme="http://www.deutschestextarchiv.de/doku/klassifikation#dwds1sub"]'));
-
-$data{createdate} = `date +'%d-%m-%Y %H:%M:%S'`;
-$data{createdate} =~ s/\n//g;
-
-
-$data{resp} = '';
-my @respStmts = getNodes('//teiHeader/fileDesc/titleStmt/respStmt');
-while(@respStmts > 1){
-	$data{resp} = $data{resp}.shift(@respStmts)."\n";
-}
-
-#TODO: just one editorialDecl??
-my @editorialDecls = getNodes('//teiHeader/encodingDesc/editorialDecl');
-if(@editorialDecls){
-	$data{txt} = $editorialDecls[0];
-}
-
-#TODO: just one msIdentifier??
-my @msIdentifiers = getNodes('//teiHeader/fileDesc/sourceDesc/msDesc/msIdentifier');
-if(@msIdentifiers){
-	$data{msidentifier} = $msIdentifiers[0];
-}
-
-#TODO: all in <licence>?
-my @licence = getNodes('//teiHeader/fileDesc/publicationStmt/availability/licence');
-if(@licence){
-	$data{license} = $licence[0];
-}
-
-#DEBUG
-#print "DATA:\n";
-#printMap(%data);
-#print "\n";
-
-
-$data{sql} = generateSqlInsert('book', 'id_book', 'title', 'subtitle', 'autor1_prename', 'autor1_lastname', 'autor1_syn_names', 'autor2_prename', 'autor2_lastname', 'autor2_syn_names', 'autor3_prename', 'autor3_lastname', 'autor3_syn_names', 'availability ', 'year', 'dta_pub_date', 'dta_pub_location', 'dta_pub_verlag','umfang', 'umfang_normiert', 'band_zaehlung', 'band_alphanum', 'autor1_pnd','autor2_pnd','autor3_pnd', 'dta_reihe_titel', 'dta_reihe_jahrgang', 'dta_reihe_band', 'dta_seiten', 'dta_bibl_angabe', 'ready', 'uebersetzer', 'dta_uebersetzer', 'publisher', 'dta_comment2');
-$data{sql} = $data{sql}.";\n".generateSqlInsert('metadaten','id_book', 'genre', 'type', 'untergenre', 'dirname', 'schriftart', 'prioritaet', 'planung', 'startseite', 'dwds_kategorie1', 'dwds_unterkategorie1');
-$data{sql} = $data{sql}.";\n".generateSqlInsert('sources','id_book', 'source'); 
-$data{sql} = $data{sql}.";\n".'INSERT INTO open_tasks (id_book, id_task, createdate) SELECT '.$data{id_book}.', max(id_task) +1, '.qut($data{createdate}).' FROM open_tasks;';
-
-#DEBUG
-#print generateSqlInsert('book', 'id_book', 'title', 'subtitle', 'autor1_prename', 'autor1_lastname', 'prioritaet');
-
-
-if(not @warnings){
-	# could add warning...
-	writeData('resp', 'txt', 'msidentifier', 'license', 'sql');	
-}
-
-if(@warnings){
-	print "WARNINGS:\n";
-	while(@warnings){
-		print shift(@warnings)."\n";		
-	}
-}else{
-	print $data{sql};	
-}
 
 __DATA__
 #TODO:
